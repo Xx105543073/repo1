1ddd89780d23ffae793cf7a5f6b5053962da54c4
admin <105>
2020-04-06 15:09:03 +0800

    deleteBlog

--- springboot-blog/src/main/java/com/xc/blog/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xc.blog.dao;
-
-import com.xc.blog.po.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-    User findByUsernameAndPassword(String username,String password);
-}
